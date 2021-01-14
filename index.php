3f582aa5551ee5d63b233c733ea1ac9df224073d
jcigama <jcigama77@yahoo.com>
2021-01-14 15:27:07 -0500

    Adding index page

--- index.php
+++ index.php
@@ -12,5 +12,10 @@ $f3 = Base::instance();
 $f3 = new Base();
 $f3->set('DEBUG', 3);
 
+//Define a default route (home page)
+$f3->route('GET /', function() {
+    echo "<h1>Hello, world!</h1>";
+});
+
 //Run fat free
 $f3->run();
