b3c405ad65bb01b1966be6ee10793a0ddd045146
jcigama <jcigama77@yahoo.com>
2021-01-14 14:50:58 -0500

    Committing index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Run fat free
+$f3->run();
